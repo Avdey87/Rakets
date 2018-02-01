475b6a38315d9f9e13b2a53f2d6d167ebf77b6bf
avdey <avdey87@gmail.com>
2018-02-01 14:33:35 +0300

    123

---
+++ app/src/main/java/com/aavdeev/rakets/Connect.java
@@ -0,0 +1,63 @@
+package com.aavdeev.rakets;
+
+
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connect {
+    private static final String TAG = "Connect";
+    private static Uri uri = Uri
+            .parse("https://api.spacexdata.com/v2/launches?launch_year=2017")
+            .buildUpon()
+            .appendQueryParameter("method", "flickr.photos.getRecent")
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s")
+            .build();
+
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() + " : with " + urlSpec);
+            }
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer,0,bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        }finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString (String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+
+
+
+
+
+}
+
+
--- app/src/main/java/com/aavdeev/rakets/RaketsActivity.java
+++ app/src/main/java/com/aavdeev/rakets/RaketsActivity.java
@@ -1,69 +1,60 @@
 package com.aavdeev.rakets;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RaketsActivity extends Activity {
 
-    TextView tvText;
-    final String textSource = "https://api.spacexdata.com/v2/launches?launch_year=2017";
+    private final String TAG = "MainActivity";
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rakets);
 
-        tvText = (TextView) findViewById(R.id.tvText);
-        tvText.setMovementMethod(new ScrollingMovementMethod());
-
-        new FileReadTask().execute();
-
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(RaketsActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        requestJsonObject();
     }
 
-    private class FileReadTask extends AsyncTask<Void, Void, Void> {
-        String textResult;
-
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            URL textUrl;
-            try {
-                textUrl = new URL(textSource);
-
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(textUrl.openStream()));
-                String StringBuffer;
-                String stringText = "";
-                while ((StringBuffer = bufferedReader.readLine()) != null) {
-                    stringText += StringBuffer;
-                }
-                bufferedReader.close();
-                textResult = stringText;
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                textResult = e.toString();
-            } catch (IOException e) {
-                e.printStackTrace();
-                textResult = e.toString();
+    private void requestJsonObject() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.spacexdata.com/v2/launches?launch_year=2017";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Response " + response);
+                GsonBuilder builder = new GsonBuilder();
+                Gson mGson = builder.create();
+                List<ItemObject> posts = new ArrayList<ItemObject>();
+                posts = Arrays.asList(mGson.fromJson(response, ItemObject[].class));
+                adapter = new RecyclerViewAdapter(RaketsActivity.this, posts);
+                recyclerView.setAdapter(adapter);
             }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            tvText.setText(textResult);
-            super.onPostExecute(result);
-        }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "Error " + error.getMessage());
+            }
+        });
+        queue.add(stringRequest);
     }
-
 }
